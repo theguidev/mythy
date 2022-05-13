e63814ab4e03bb13edda202b3707f8c32fba024d
theguidev <princecronoszeus@gmail.com>
2022-05-12 21:27:08 -0300

    Fix: Attempts System

---
+++ attempts.ts
@@ -0,0 +1,224 @@
+export const mockAttempts = [
+  {
+    id: 0,
+    disabled: false,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    disabled: true,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    disabled: true,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    disabled: true,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    disabled: true,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    disabled: true,
+    value: "",
+    inputs: [
+      {
+        id: 0,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 1,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 2,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 3,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+      {
+        id: 4,
+        value: "",
+        rightPlace: false,
+        exists: false,
+      },
+    ],
+  },
+];
