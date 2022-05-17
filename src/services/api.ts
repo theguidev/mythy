7aff717a5d8643561be9a39bc9bec3e5a1e27a45
theguidev <princecronoszeus@gmail.com>
2022-05-17 20:10:52 -0300

    Fix: API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: process.env.NODE_ENV !== "development" ? "https://mythy.vercel.app/api" : "http://localhost:3000/api"
-  baseURL: "http://localhost:3000/api"
+  baseURL: process.env.API_URL
 })
